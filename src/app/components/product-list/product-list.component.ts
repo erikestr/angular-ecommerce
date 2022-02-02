cf850a2334f1dbec0927a8da23a1ec845b6a0890
Erik E <erikestrada37@outlook.com>
2022-02-01 21:13:05 -0600

    add selecting page size

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
 
   //new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
 
@@ -97,7 +97,7 @@ export class ProductListComponent implements OnInit {
     processResult() {
       //Error
       //return data => {
-        
+
       //QUICK FIX
       return (data: { _embedded: { products: Product[]; }; page: { number: number; size: number; totalElements: number; }; }) => {
         this.products = data._embedded.products;
@@ -105,5 +105,10 @@ export class ProductListComponent implements OnInit {
         this.thePageSize = data.page.size;
         this.theTotalElements = data.page.totalElements;
     };
+  }
+  updatePageSize(event: any){
+    this.thePageSize = event.target.value;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
