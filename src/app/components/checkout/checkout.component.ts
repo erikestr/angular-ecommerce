58faf0cb169493781d293b6831d71cd79a8c7e06
erikestr <erikestrada37@outlook.com>
2022-02-03 13:30:13 -0600

    update component checkout logs

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -93,6 +93,9 @@ export class CheckoutComponent implements OnInit {
     console.log("Handlign the submit button");
     console.log(this.checkoutFormGroup?.get('customer')?.value);
     console.log(`The email address is: ${this.checkoutFormGroup?.get('customer')?.value.email}`);
+
+    console.log("The shipping addres country is " + this.checkoutFormGroup?.get('shippingAddress')?.value.country.name);
+    console.log("The shipping addres state is " + this.checkoutFormGroup?.get('shippingAddress')?.value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event : any){
@@ -133,8 +136,8 @@ export class CheckoutComponent implements OnInit {
     const countryCode = formGroup?.value.country.code;
     const countryName = formGroup?.value.country.name;
 
-    console.log(`${formGroup} country code: ${countryCode}`);
-    console.log(`${formGroup} country code: ${countryName}`);
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
 
     this.luv2ShopService.getStates(countryCode).subscribe(
       data => {
