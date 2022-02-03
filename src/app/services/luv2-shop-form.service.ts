5a297636340c5ef7d9ff04667410c3b5656deaa4
erikestr <erikestrada37@outlook.com>
2022-02-03 10:47:56 -0600

    update service luv2-shop-form getMonths and getYears

--- src/app/services/luv2-shop-form.service.ts
+++ src/app/services/luv2-shop-form.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,27 @@ import { Injectable } from '@angular/core';
 export class Luv2ShopFormService {
 
   constructor() { }
+
+  getCreditCardMonths(startMonth: number): Observable<number[]>{
+      let data: number[] = [];
+
+      for(let theMonth = startMonth; theMonth <=12; theMonth){
+        data.push(theMonth);
+      }
+
+      return of(data);
+  }
+
+  getCreditCardYears(): Observable<number[]>{
+    let data: number[] = [];
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+    
+    for(let theYear = startYear; theYear <= endYear; theYear++){
+      data.push(theYear);
+    }
+
+    return of(data);
+  }
 }
