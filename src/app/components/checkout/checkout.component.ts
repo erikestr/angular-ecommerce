0ca7e3dcbb8afde8961904acf144ef8bcdb31397
erikestr <erikestrada37@outlook.com>
2022-02-03 13:13:34 -0600

    update component checkout getSates

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
@@ -19,7 +20,10 @@ export class CheckoutComponent implements OnInit {
   creditCardYears: number[] = [];
 
   //Luv2ShopService --> Countries and States
-  countries: Country[] = []
+  countries: Country[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
               private luv2ShopService: Luv2ShopFormService) { }
@@ -118,6 +122,29 @@ export class CheckoutComponent implements OnInit {
       data => {
         console.log("Retrieved credit card months: " + JSON.stringify(data));
         this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getSates(formGroupName: string){
+
+    const formGroup = this.checkoutFormGroup?.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroup} country code: ${countryCode}`);
+    console.log(`${formGroup} country code: ${countryName}`);
+
+    this.luv2ShopService.getStates(countryCode).subscribe(
+      data => {
+        if(formGroupName === 'shippingAddress'){
+          this.shippingAddressStates = data;
+        }else{
+          this.billingAddressStates = data;
+        }
+
+        formGroup?.get('state')?.setValue(data[0]);
       }
     );
   }
