0231c20799c5b8a0785931f11f74be0fababcf30
Erik E <erikestrada37@outlook.com>
2022-02-03 21:12:47 -0600

    update component checkout credit Card validators

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -80,12 +80,17 @@ export class CheckoutComponent implements OnInit {
                                 Luv2ShopValidators.notOnlyWhitespace])
       }),
       creditCard: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
+        cardType: new FormControl('', [ Validators.required ]),
+        nameOnCard: new FormControl('', 
+                              [ Validators.required, 
+                                Validators.minLength(2),
+                                Luv2ShopValidators.notOnlyWhitespace]),
+        cardNumber: new FormControl('', [ Validators.pattern('[0-9]{16}') ]),
+        securityCode: new FormControl('', [ Validators.pattern('[0-9]{3}') ]),
         expirationMonth: [''],
-        expirationYear: [''],
+        expirationYear: ['']
+        // expirationMonth: new FormControl('', [ Validators.required ]),
+        // expirationYear: new FormControl('', [ Validators.required ])
       })
     });
 
@@ -134,6 +139,14 @@ export class CheckoutComponent implements OnInit {
   get billingAddressCountry(){ return this.checkoutFormGroup?.get('billingAddress.country'); }
   get billingAddressState(){ return this.checkoutFormGroup?.get('billingAddress.state'); }
   get billingAddressZipCode(){ return this.checkoutFormGroup?.get('billingAddress.zipCode'); }
+
+  
+  get creditCardType(){ return this.checkoutFormGroup?.get('creditCard.cardType'); }
+  get creditCardNameOnCard(){ return this.checkoutFormGroup?.get('creditCard.nameOnCard'); }
+  get creditCardNumber(){ return this.checkoutFormGroup?.get('creditCard.cardNumber'); }
+  get creditCardSecurityCode(){ return this.checkoutFormGroup?.get('creditCard.securityCode'); }
+  // get creditCardExpirationMonth(){ return this.checkoutFormGroup?.get('creditCard.expirationMonth'); }
+  // get creditCardExpirationYear(){ return this.checkoutFormGroup?.get('creditCard.expirationYear'); }
 
   onSubmit(){
     console.log("Handlign the submit button");
