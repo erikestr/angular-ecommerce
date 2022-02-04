24f6b0207003876d834b9e5d7418074fb1537b01
Erik E <erikestrada37@outlook.com>
2022-02-03 20:33:31 -0600

    update component checkout lastName notOnlyWhitespace

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -39,7 +39,10 @@ export class CheckoutComponent implements OnInit {
                               [ Validators.required, 
                                 Validators.minLength(2),
                                 Luv2ShopValidators.notOnlyWhitespace]),
-        lastName:  new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName:  new FormControl('', 
+                              [ Validators.required, 
+                                Validators.minLength(2),
+                                Luv2ShopValidators.notOnlyWhitespace]),
         email:  new FormControl('', 
                               [ Validators.required, 
                                 Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
