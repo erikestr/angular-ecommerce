d7b9a77b3b8b83b45fdd67c1492b6320c505ee96
erikestrada37@outlook.com <piripitiflautica99>
2022-02-05 18:13:04 -0600

    update config okta

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,2 +1,9 @@
-export interface MyAppConfig {
+export default {
+    oidc: {
+        clientId: '0oa3t2a8l9p1L8jXD5d7',
+        issuer: 'https://dev-49239349.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
 }
+    
