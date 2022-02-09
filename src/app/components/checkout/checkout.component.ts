54d9a2d2c923a515b22dde50c3ba0a1b9bceb941
Erik E <erikestrada37@outlook.com>
2022-02-08 19:28:10 -0600

    update component checkout email receipt to stripe & update class payment info

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -280,6 +280,7 @@ export class CheckoutComponent implements OnInit {
     // compute payment info
     this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency = "USD";
+    this.paymentInfo.receiptEmail = purchase.customer.email;
 
     // if valid form then
     // - create payment intent
